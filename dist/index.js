38c8a2f3b1cd2509717e01e16da90a6234119dd1
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-11-14 00:22:07 -0300

    fix: ignoring files

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { config } from 'dotenv';
-config();
-var server = express();
-server.listen(process.env.PORT, function () { console.log('Listening on port 4000...'); });
