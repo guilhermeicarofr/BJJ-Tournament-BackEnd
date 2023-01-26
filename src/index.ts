0eeea613525785a383ecb593badd70de75b6597d
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-01-26 05:59:44 -0300

    infrastructure: setting up docker related files, new scripts and envs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import { config } from 'dotenv';
 config();
 
+const PORT = process.env.PORT || 5000;
+
 import athleteControllers from './controllers/athlete.controllers.js';
 import { validateSchema } from './middlewares/validation.middlewares.js';
 import { athleteInputSchema } from './schemas/schemas.js';
@@ -12,12 +14,14 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.get('/athletes', athleteControllers.readAll);
-server.post('/athletes', validateSchema(athleteInputSchema), athleteControllers.createOne);
-server.put('/athletes/:id', validateSchema(athleteInputSchema), athleteControllers.updateOne);
-server.delete('/athletes/:id', athleteControllers.removeOne);
+server.get('/health', (req, res) => console.log('running'));
 
-server.get('/tournament/:belt/:age/:weight', listTournament);
-server.get('/tournament/:belt/absolute', listAbsolute);
+//server.get('/athletes', athleteControllers.readAll);
+// server.post('/athletes', validateSchema(athleteInputSchema), athleteControllers.createOne);
+// server.put('/athletes/:id', validateSchema(athleteInputSchema), athleteControllers.updateOne);
+// server.delete('/athletes/:id', athleteControllers.removeOne);
 
-server.listen(process.env.PORT, () => {console.log(`Listening on port ${process.env.PORT}...`)});
+// server.get('/tournament/:belt/:age/:weight', listTournament);
+// server.get('/tournament/:belt/absolute', listAbsolute);
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
